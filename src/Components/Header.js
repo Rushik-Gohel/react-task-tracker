e1c8d26b4dbddd7cdc6094314dc6ea5cab16e563
rushik <rushikgohel@gmail.com>
2022-02-12 16:40:02 +0530

    Added async await for json db

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,15 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Header = ({ title }) => {
-
-  const onClick = () => {
-    console.log('clicked')
-  }
-
+const Header = ({ title, onAdd, showAddTask }) => {
   return (
     <header className="Header">
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick}/>
+      <Button
+        color={showAddTask ? "red" : "green"}
+        text={showAddTask ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
   );
 };
